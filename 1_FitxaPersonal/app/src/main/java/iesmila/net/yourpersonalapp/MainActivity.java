933ee066231d8a1f941a41c6f8e197a14bff7c4c
Bernat <bernat.orellana@gmail.com>
2020-01-24 15:17:03 +0100

    primer projecte

---
+++ 1_FitxaPersonal/app/src/main/java/iesmila/net/yourpersonalapp/MainActivity.java
@@ -0,0 +1,113 @@
+package iesmila.net.yourpersonalapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import iesmila.net.yourpersonalapp.model.Fitxa;
+
+public class MainActivity extends AppCompatActivity {
+
+    // ------------------------------------
+    //  Una variable per a cada control de l'Activity
+    // ------------------------------------
+    EditText        edtNom;
+    RadioGroup      rgoSexe;
+    RadioButton     rdoHome;
+    RadioButton     rdoDona;
+    DatePicker      dtpData;
+    Button          btnNext;
+    Button          btnPrev;
+    ImageView       imgFoto;
+    // ------------------------------------
+
+    Fitxa fitxaActual;
+    int indexFitxaActual=-1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //---------------------------------------
+        edtNom = findViewById(R.id.edtNom);
+        rgoSexe = findViewById(R.id.rgoSexe);
+        rdoHome = findViewById(R.id.rdoHome);
+        rdoDona = findViewById(R.id.rdoDona);
+        dtpData = findViewById(R.id.dtpData);
+        btnNext = findViewById(R.id.btnNext);
+        btnPrev = findViewById(R.id.btnPrev);
+        imgFoto = findViewById(R.id.imgFoto);
+        //---------------------------------------
+        // Prenem la primera fitxa de la llista
+        //fitxaActual = Fitxa.getFitxes().get(1);
+        indexFitxaActual = 0;
+        mostrarFitxaActual();
+        //----------------------------------------
+        // Programar els botons
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                indexFitxaActual =  (indexFitxaActual+1)%Fitxa.getFitxes().size();
+                mostrarFitxaActual();
+            }
+        });
+        btnPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                indexFitxaActual--;
+                if(indexFitxaActual<0) indexFitxaActual = Fitxa.getFitxes().size()-1;
+                mostrarFitxaActual();
+            }
+        });
+        //----------------------------------------------
+        // Events del EditText
+        edtNom.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+             fitxaActual.setNom(s.toString());
+            }
+        });
+
+    }
+
+    private void mostrarFitxaActual() {
+        // Verifiquem abans que l'index de la fitxa no surti de mare.
+        if(indexFitxaActual>=0 && indexFitxaActual<Fitxa.getFitxes().size()) {
+
+            fitxaActual = Fitxa.getFitxes().get(indexFitxaActual);
+
+            edtNom.setText(fitxaActual.getNom());
+            if (fitxaActual.isEsHome()) {
+                rgoSexe.check(R.id.rdoHome);
+            } else {
+                rgoSexe.check(R.id.rdoDona);
+            }
+            imgFoto.setImageResource(fitxaActual.getRecursImatge());
+
+            dtpData.updateDate(
+                    fitxaActual.getData().getYear(),
+                    fitxaActual.getData().getMonth() - 1,
+                    fitxaActual.getData().getDate()
+            );
+        }
+    }
+}
---
+++ 1_FitxaPersonal/app/src/main/java/iesmila/net/yourpersonalapp/model/Fitxa.java
@@ -0,0 +1,90 @@
+package iesmila.net.yourpersonalapp.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import iesmila.net.yourpersonalapp.R;
+
+public class Fitxa {
+
+    // Atributs de la classe
+    private int id;
+    private String nom;
+    private boolean esHome;
+    private Date data;
+    private int recursImatge; // ****
+
+
+    private static List<Fitxa> fitxes;
+    public static List<Fitxa> getFitxes() {
+        if( fitxes==null) {
+            fitxes = new ArrayList<Fitxa>();
+            fitxes.add(
+                    new Fitxa(1,
+                            "Trump",
+                            true,
+                            new Date(1960, 10, 10),
+                            R.drawable.trump)
+            );
+            fitxes.add(
+                    new Fitxa(2,
+                            "Maria",
+                            false,
+                            new Date(1970, 12, 04),
+                            R.drawable.maria
+                    )
+            );
+        }
+        return fitxes;
+    }
+
+
+    public Fitxa(int id, String nom, boolean esHome, Date data, int recursImatge) {
+        this.id = id;
+        this.nom = nom;
+        this.esHome = esHome;
+        this.data = data;
+        this.recursImatge = recursImatge;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public boolean isEsHome() {
+        return esHome;
+    }
+
+    public void setEsHome(boolean esHome) {
+        this.esHome = esHome;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public int getRecursImatge() {
+        return recursImatge;
+    }
+
+    public void setRecursImatge(int recursImatge) {
+        this.recursImatge = recursImatge;
+    }
+}
